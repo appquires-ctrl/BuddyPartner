eb516a13af5c933176ba841b9ba5a78d1b18f994
Dhruv Soni <dhruvsoni9696490@email.com>
2026-07-21 14:50:02 +0530

    style: increase purple saturation in skeleton loading theme

--- lib/app/theme/skeleton_colors.dart
+++ lib/app/theme/skeleton_colors.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 class SkeletonColors {
   // Light Theme
   static const Color lightBackground = Color(0xFFFAF8FF);
-  static const Color lightBase = Color(0xFFF1EBFF);
-  static const Color lightHighlight = Color(0xFFFFFFFF);
-  static const Color lightAccent = Color(0xFFE4D8FF);
+  static const Color lightBase = Color(0xFFE5DFFF);
+  static const Color lightHighlight = Color(0xFFF1EBFF);
+  static const Color lightAccent = Color(0xFFD6C8FF);
 
   // Dark Theme
   static const Color darkBackground = Color(0xFF1A1625);
-  static const Color darkBase = Color(0xFF2A2338);
-  static const Color darkHighlight = Color(0xFF3B3150);
-  static const Color darkAccent = Color(0xFF4A3B67);
+  static const Color darkBase = Color(0xFF322745);
+  static const Color darkHighlight = Color(0xFF42345B);
+  static const Color darkAccent = Color(0xFF58457D);
 
   static Color background(bool isDark) => isDark ? darkBackground : lightBackground;
   static Color baseColor(bool isDark) => isDark ? darkBase : lightBase;
